434661ee58c3103d5d49f5c909be79ff374c59ff
JOGI VENKATA SAI KIRAN <kiranjogisk123@gmail.com>
2025-07-21 17:01:13 +0530

    modifies the old code

--- Arrays/KunalAssignements/LeetCode832.java
+++ Arrays/KunalAssignements/LeetCode832.java
@@ -1,33 +1,27 @@
-package Java_Learning.LeetCode;
-//832. Flipping an Image
-public class LeetC832 {
+import java.util.Arrays;
+
+// 832. Flipping an Image
+public class LeetCode832 {
     public static void main(String[] args) {
         int[][] image = {
-                {1,1,0},
-                {1,0,1},
-                {0,0,0}
+                {1, 1, 0},
+                {1, 0, 1},
+                {0, 0, 0}
         };
 
+        int[][] ans = flipAndInvertImage(image);
+        System.out.println(Arrays.deepToString(ans));
     }
-    static  int[][] flipAndInvertImage(int[][] image) {
-        for(int[] row : image){
-            int start = 0;
-            int end = row.length - 1;
 
-            while(start < end){
-                int temp = row[start];
-                row[start] = row[end];
-                row[end] = temp;
-
-                start++;
-                end--;
+    static int[][] flipAndInvertImage(int[][] image) {
+        int n = image.length;
+        for (int[] row : image) {
+            for (int i = 0; i < (n + 1) / 2; i++) {
+                int temp = row[i] ^ 1;
+                row[i] = row[n - 1 - i] ^ 1;
+                row[n - 1 - i] = temp;
             }
         }
-        for(int[] row : image){
-            for(int[] col : image){
-                image[row][col] ^= 1;
-            }
-        }
-        return ;
+        return image;
     }
 }
